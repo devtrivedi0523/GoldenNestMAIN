2cba115eec64e593d2cf64d5489dd3eb72ba61b2
Dev Trivedi <dev.trivedi0523@gmail.com>
2025-12-12 17:04:45 -0800

    added www

--- goldennest/src/main/java/com/Backend/GoldenNest/config/SecurityConfig.java
+++ goldennest/src/main/java/com/Backend/GoldenNest/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8000", "https://golden-nest-main.vercel.app", "https://thegoldennest.co.uk"));
+        cfg.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:8000", "https://golden-nest-main.vercel.app", "https://www.thegoldennest.co.uk"));
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         cfg.setAllowCredentials(true);
